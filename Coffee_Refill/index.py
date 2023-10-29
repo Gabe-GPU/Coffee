5f4a0313df9a4ae61b104e120f4795549f1ce53f
Gabe M <gabriele.luca.malatesta@gmail.com>
2023-10-28 21:14:55 -0400

    first_commit

---
+++ Coffee_Refill/index.py
@@ -0,0 +1,8 @@
+def empty_coffee(coffee_refill):  
+    if coffee_refill == 3:  
+        print("Refilled!")  
+    else:  
+        print("Not yet refilled.")  
+
+for x in range(1, 4):  
+    empty_coffee(x) 
